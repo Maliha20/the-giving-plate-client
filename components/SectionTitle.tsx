b05a42664a129a03f51dc5508399f9cf1097b78a
maliha20 <gulista.marium20@gmail.com>
2023-07-11 17:18:01 +0600

    section title changed

--- components/SectionTitle.tsx
+++ components/SectionTitle.tsx
@@ -13,7 +13,7 @@ const SectionTitle: React.FC<SectionTitleProps> = ({ title }) => {
         initial={{ y: "-100%" }}
         whileInView={{ y: 0 }}
         transition={{ ease: "easeInOut", duration: 0.75 }}
-        className="text-2xl md:text-4xl font-semibold text-white text-center uppercase mb-10"
+        className="text-2xl md:text-4xl font-semibold text-black text-center uppercase mb-10"
       >
         {title}
       </motion.h2>
