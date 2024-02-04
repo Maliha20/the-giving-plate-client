de69f991b16b5f16372c7114c600f3f2c92a4f4e
maliha20 <gulista.marium20@gmail.com>
2024-02-04 20:52:00 +0600

    cloudinary added

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["images.pexels.com", "images.unsplash.com", "res.imgbb.com"],
+    domains: ["images.pexels.com", "images.unsplash.com", "res.imgbb.com", "res.cloudinary.com"],
   },
 };
 
