7a77dfb3761cc7347d3496ce50cb3eb388b74c2b
maliha20 <gulista.marium20@gmail.com>
2024-02-04 20:09:30 +0600

    domain added

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["images.pexels.com", "images.unsplash.com", "images.localhost.com"],
+    domains: ["images.pexels.com", "images.unsplash.com", "images.localhost.com", "image.imgbb.com"],
   },
 };
 
